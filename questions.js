ce1a2022f9204414ae73901e20bf5b101f07968e
EcdadComputer <ulash-72@hotmail.com>
2020-02-29 03:48:03 +0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -4,7 +4,7 @@ questions = [
     {
         "question": "''Sovereignty unconditionally belongs to the Nation.''",
         "i18n": {
-            "en": "''Egemenlik kayıtsız ve şartsız milletindir.''", 
+            "de": "''Egemenlik kayıtsız ve şartsız milletindir.''", 
         },
         "effect": {
             "a": 10,
@@ -18,7 +18,7 @@ questions = [
     {
         "question": "''The freedom of conscience should be recognised as absolute and unassailable right of the individual.''",
         "i18n": {
-            "en": "''Vicdan hürriyeti mutlak ve taarruz edilmez, ferdin tabi haklarının en mühimlerinden tanınmalıdır.''",
+            "de": "''Vicdan hürriyeti mutlak ve taarruz edilmez, ferdin tabi haklarının en mühimlerinden tanınmalıdır.''",
  
         },
         "effect": {
@@ -33,7 +33,7 @@ questions = [
     {
         "question": "''Republic is an administration based on moral virtue. Monarchy is an administration based on fear and threat. The Republican administration trains people with virtue and dignity. Monarchy raises cowardly, low, miserable and infamous people because it is based on fear and threat. The difference is just that.''",
         "i18n": {
-            "en": "''Cumhuriyet, ahlâkî fazilete dayanan bir idaredir. Cumhuriyet fazilettir. Sultanlık, korku ve tehdide dayanan bir idaredir. Cumhuriyet idaresi faziletli ve namuslu insanlar yetiştirir. Sultanlık, korkuya ve tehdide dayandığı için korkak, alçak, sefil ve rezil insanlar yetiştirir. Aradaki fark bunlardan ibarettir.''",
+            "de": "''Cumhuriyet, ahlâkî fazilete dayanan bir idaredir. Cumhuriyet fazilettir. Sultanlık, korku ve tehdide dayanan bir idaredir. Cumhuriyet idaresi faziletli ve namuslu insanlar yetiştirir. Sultanlık, korkuya ve tehdide dayandığı için korkak, alçak, sefil ve rezil insanlar yetiştirir. Aradaki fark bunlardan ibarettir.''",
         },
         "effect": {
             "a": 10,
@@ -47,7 +47,7 @@ questions = [
     {
         "question": "The Parliament has the legislative power on behalf of the Nation. This authority cannot be delegated.",
         "i18n": {
-            "en": "Yasama yetkisi Türk Milleti adına Türkiye Büyük Millet Meclisinindir. Bu yetki devredilemez.",
+            "de": "Yasama yetkisi Türk Milleti adına Türkiye Büyük Millet Meclisinindir. Bu yetki devredilemez.",
         },
         "effect": {
             "a": 10,
@@ -61,7 +61,7 @@ questions = [
     {
         "question": "Military is the protector of the regime.",
         "i18n": {
-            "en": "Asker, rejimin yılmaz bekçisidir.",
+            "de": "Asker, rejimin yılmaz bekçisidir.",
         },
         "effect": {
             "a": -5,
@@ -76,7 +76,7 @@ questions = [
     {
         "question": "''The peasant is our benefactor. We should consider the welfare of this people.''",
         "i18n": {
-            "en": "''Köylü, hepimizin velinimetidir. Bu necip unsurun refahını düşüneceğiz.''",
+            "de": "''Köylü, hepimizin velinimetidir. Bu necip unsurun refahını düşüneceğiz.''",
         },
         "effect": {
             "a": 0,
@@ -91,7 +91,7 @@ questions = [
     {
         "question": "There is only one bias: Support for republicanism, support for intellectual and social reform. At this point, i don't want to think anybody who doesn't support this bias.",
         "i18n": {
-            "en": "''Bütün dünya bilsin ki benim için bir taraflılık vardır: Cumhuriyet taraftarlığı, fikri ve sosyal inkılap taraftarlığı. Bu noktada, yeni Türkiye topluluğunda bir ferdi, hariç düşünmek istemiyorum.''",
+            "de": "''Bütün dünya bilsin ki benim için bir taraflılık vardır: Cumhuriyet taraftarlığı, fikri ve sosyal inkılap taraftarlığı. Bu noktada, yeni Türkiye topluluğunda bir ferdi, hariç düşünmek istemiyorum.''",
         },
         "effect": {
             "a": 0,
@@ -106,7 +106,7 @@ questions = [
 	{
         "question": "''Establishing a modern state means that the nation knows how to live humanly and learn what it is like to live humanly''.",
         "i18n": {
-            "en": "''Çağdaş bir cumhuriyet kurmak demek, milletin insanca yaşamasını bilmesi, insanca yaşamanın neye bağlı olduğunu öğrenmesi demektir.''",
+            "de": "''Çağdaş bir cumhuriyet kurmak demek, milletin insanca yaşamasını bilmesi, insanca yaşamanın neye bağlı olduğunu öğrenmesi demektir.''",
         },
         "effect": {
             "a": 0,
@@ -121,7 +121,7 @@ questions = [
     {
         "question": "''The well-being of humanity should replace hunger and oppression. People of the world should be educated in a way that takes them away from jealousy, greed and hatred.''",
         "i18n": {
-            "en": "''İnsanlığın bütününün refahı, açlık ve baskının yerine geçmelidir. Dünya vatandaşları, kıskançlık, açgözlülük ve kinden uzaklaşacak şekilde eğitilmelidir.''",
+            "de": "''İnsanlığın bütününün refahı, açlık ve baskının yerine geçmelidir. Dünya vatandaşları, kıskançlık, açgözlülük ve kinden uzaklaşacak şekilde eğitilmelidir.''",
         },
         "effect": {
             "a": 0,
@@ -136,7 +136,7 @@ questions = [
 	{
         "question": "The Kemalist populism program should be taken further to ensure absolute social equality.",
         "i18n": {
-            "en": "Atatürkçü halkçılık programı daha da ileriye götürülmeli, mutlak toplumsal eşitlik sağlanmalıdır.",
+            "de": "Atatürkçü halkçılık programı daha da ileriye götürülmeli, mutlak toplumsal eşitlik sağlanmalıdır.",
         },
         "effect": {
             "a": 0,
@@ -153,7 +153,7 @@ questions = [
     {
         "question": "''Unless absolutely necessary, the state cannot interfere to the market...''",
         "i18n": {
-            "en": "''Kesin zaruret olmadıkça piyasalara karışılmaz...''",
+            "de": "''Kesin zaruret olmadıkça piyasalara karışılmaz...''",
         },
         "effect": {
             "a": 0,
@@ -168,7 +168,7 @@ questions = [
     {
         "question": "''...nevertheless, no market is stray.''",
         "i18n": {
-            "en": "''...bununla beraber hiçbir piyasa da başıboş değildir.''",
+            "de": "''...bununla beraber hiçbir piyasa da başıboş değildir.''",
         },
         "effect": {
             "a": 0,
@@ -183,7 +183,7 @@ questions = [
     {
         "question": "''Our people are naturally statist, who sees a right to demand all kinds of needs from the state. In this respect, there is a complete harmony with the temperament of our nation and the program of our party. We will walk in this direction and there is no doubt that we will be successful.''",
         "i18n": {
-            "en": "''Halkımız doğal olarak devletçidir ki, her türlü gereksinimi devletten istemek için kendisinde bir hak görüyor. Bu itibarla milletimizin mizacıyla partimizin programında tam bir uyum vardır. Bu doğrultudan yürüyeceğiz ve başarılı olacağımızda şüphe yoktur.''",
+            "de": "''Halkımız doğal olarak devletçidir ki, her türlü gereksinimi devletten istemek için kendisinde bir hak görüyor. Bu itibarla milletimizin mizacıyla partimizin programında tam bir uyum vardır. Bu doğrultudan yürüyeceğiz ve başarılı olacağımızda şüphe yoktur.''",
         },
         "effect": {
             "a": 0,
@@ -198,7 +198,7 @@ questions = [
     {
         "question": "In order to increase the production of all kinds in the country, private enterprise is important.",
         "i18n": {
-            "en": "Memlekette her çeşit üretimin daha fazla olması için, şahsi teşebbüs önemlidir.",
+            "de": "Memlekette her çeşit üretimin daha fazla olması için, şahsi teşebbüs önemlidir.",
         },
         "effect": {
             "a": 0,
@@ -213,7 +213,7 @@ questions = [
     {
         "question": "''In trade, earning a lot does not apply. The principle of winning correctly and cleanly applies.''",
         "i18n": {
-            "en": "''Ticarette çok kazanmak değil, doğru ve temiz kazanmak İlkesi geçerlidir.''",
+            "de": "''Ticarette çok kazanmak değil, doğru ve temiz kazanmak İlkesi geçerlidir.''",
         },
         "effect": {
             "a": 0,
@@ -228,7 +228,7 @@ questions = [
     {
         "question": "''Everyone should know their national duties and responsibilities, and it should be their duty to think and work on the issues of the country with that thought.''",
         "i18n": {
-            "en": "''Herkes ulusal görevini ve sorumluluğunu bilmeli, memleket meseleleri üzerinde o düşünceyle, düşünüp çalışmayı görev edinmelidir.''",
+            "de": "''Herkes ulusal görevini ve sorumluluğunu bilmeli, memleket meseleleri üzerinde o düşünceyle, düşünüp çalışmayı görev edinmelidir.''",
         },
         "effect": {
             "a": 0,
@@ -242,7 +242,7 @@ questions = [
     {
         "question": "''Great nations do great things.''",
         "i18n": {
-            "en": "''Büyük şeyleri büyük milletler yapar.''",
+            "de": "''Büyük şeyleri büyük milletler yapar.''",
         },
         "effect": {
             "a": 0,
@@ -256,7 +256,7 @@ questions = [
     {
         "question": "War is a murder, unless the nation's life faces danger.",
         "i18n": {
-            "en": "Ulusun yaşamı tehlikeyle karşı karşıya kalmadıkça savaş bir cinayettir.",
+            "de": "Ulusun yaşamı tehlikeyle karşı karşıya kalmadıkça savaş bir cinayettir.",
         },
         "effect": {
             "a": 0,
@@ -270,7 +270,7 @@ questions = [
     {
         "question": "''There are a number of economic problems that we cannot solve with our own resources and with our own capital alone. We have to look for friends who will help us.''",
         "i18n": {
-            "en": "''Birtakım ekonomik sorunlar vardır ki, biz bunları kendi kaynaklarımızla ve yalnız kendi sermayemizle çözümleye-meyiz. Bize yardım edecek dostlar aramağa mecburuz.''",
+            "de": "''Birtakım ekonomik sorunlar vardır ki, biz bunları kendi kaynaklarımızla ve yalnız kendi sermayemizle çözümleye-meyiz. Bize yardım edecek dostlar aramağa mecburuz.''",
         },
         "effect": {
             "a": 0,
@@ -284,7 +284,7 @@ questions = [
     {
         "question": "''To give my life for the sake of national domination, I owe me conscience and honor.''",
         "i18n": {
-            "en": "''Millî hâkimiyet uğrunda canımı vermek, benim için vicdan ve namus borcu olsun.''",
+            "de": "''Millî hâkimiyet uğrunda canımı vermek, benim için vicdan ve namus borcu olsun.''",
         },
         "effect": {
             "a": 0,
@@ -299,7 +299,7 @@ questions = [
     {
         "question": "''The most accurate, most genuine cult, cult of civilization''.",
         "i18n": {
-            "en": "''En doğru, en hakiki tarikat, medeniyet tarikatıdır.''",
+            "de": "''En doğru, en hakiki tarikat, medeniyet tarikatıdır.''",
         },
         "effect": {
             "a": 0,
@@ -313,7 +313,7 @@ questions = [
     {
         "question": "''There is religion and it is necessary...''",
         "i18n": {
-            "en": "''Din vardır ve lâzımdır...''",
+            "de": "''Din vardır ve lâzımdır...''",
         },
         "effect": {
             "a": 0,
@@ -328,7 +328,7 @@ questions = [
     {
         "question": "''Religion is a matter of conscience. Everyone is free to obey the orders of their conscience.''",
         "i18n": {
-            "en": "''Din bir vicdan meselesidir. Herkes vicdanının emrine uymakta serbesttir.''",
+            "de": "''Din bir vicdan meselesidir. Herkes vicdanının emrine uymakta serbesttir.''",
         },
         "effect": {
             "a": 0,
@@ -343,7 +343,7 @@ questions = [
     {
         "question": "''The malignants who led us to the wrong path have taken refuge in the veil of religion many times.''",
         "i18n": {
-            "en": "''Bizi yanlış yola sevk eden habisler, biliniz ki çok kere din perdesine bürünmüşlerdir.''",
+            "de": "''Bizi yanlış yola sevk eden habisler, biliniz ki çok kere din perdesine bürünmüşlerdir.''",
         },
         "effect": {
             "a": 0,
@@ -358,7 +358,7 @@ questions = [
     {
         "question": "''I do not have a religion, and sometimes I want all religions to grow to the bottom of the sea.''",
         "i18n": {
-            "en": "''Benim bir dinim yok ve bazen bütün dinlerin denizin dibini boylamasını istiyorum.''",
+            "de": "''Benim bir dinim yok ve bazen bütün dinlerin denizin dibini boylamasını istiyorum.''",
         },
         "effect": {
             "a": 0,
@@ -374,7 +374,7 @@ questions = [
     {
         "question": "''Our true mentor in life is science.''",
         "i18n": {
-            "en": "Hayatta en hakiki mürşit ilimdir.",
+            "de": "Hayatta en hakiki mürşit ilimdir.",
         },
         "effect": {
             "a": 0,
@@ -388,7 +388,7 @@ questions = [
     {
         "question": "''In short, the result is this: let's be brave about women, let alone all doubt ... Let their winds fill with real knowledge and art.''",
         "i18n": {
-            "en": "Sözün kısacası sonuç: Bu, kadın konusunda cesur olalım, kuşkuyu bırakalım... Onların dimağları gerçek bilgi ve sanat ile bezensin.",
+            "de": "Sözün kısacası sonuç: Bu, kadın konusunda cesur olalım, kuşkuyu bırakalım... Onların dimağları gerçek bilgi ve sanat ile bezensin.",
         },
         "effect": {
             "a": 0,
@@ -402,7 +402,7 @@ questions = [
     {
         "question": "''Walking on the path of civilization and being successful is the condition of life. Those who pause on this road, or those who are ignorant and stupid to look back, not forward on the road, are doomed to drown under the exuberant flood of general civilization.''",
         "i18n": {
-            "en": "''Uygarlık yolunda yürümek ve başarılı olmak, yaşamın şartıdır. Bu yol üzerinde duraklayanlar ya da yol üzerinde ileri değil, geriye bakmak bilgisizliği ve aymazlığında bulunanlar, genel uygarlığın coşkun seli altında boğulmaya mahkûmdur.''",
+            "de": "''Uygarlık yolunda yürümek ve başarılı olmak, yaşamın şartıdır. Bu yol üzerinde duraklayanlar ya da yol üzerinde ileri değil, geriye bakmak bilgisizliği ve aymazlığında bulunanlar, genel uygarlığın coşkun seli altında boğulmaya mahkûmdur.''",
         },
         "effect": {
             "a": 0,
@@ -416,7 +416,7 @@ questions = [
     {
         "question": "Abortion is an immoral act that should be banned or significantly limited.",
         "i18n": {
-            "en": "''Sanatsız kalan bir milletin hayat damarlarından biri kopmuş demektir.''",
+            "de": "''Sanatsız kalan bir milletin hayat damarlarından biri kopmuş demektir.''",
         },
         "effect": {
             "a": 0,
@@ -430,7 +430,7 @@ questions = [
     {
         "question": "''First of all, our ancestors are fish because people come out of the waters like nits.''",
         "i18n": {
-            "en": "''İnsanlar sürfeler gibi sulardan çıktıkları için önce, ilk önce ceddimiz balıktır...''",
+            "de": "''İnsanlar sürfeler gibi sulardan çıktıkları için önce, ilk önce ceddimiz balıktır...''",
             "ko": "일반적으로 종교는 긍정적인 영향을 미치며 사회적인 영향력을 유지해야 한다.",
 	    "ru": "Религии в общем имеют положительный эффект и должны оставаться влиятельными в обществе."
         },
